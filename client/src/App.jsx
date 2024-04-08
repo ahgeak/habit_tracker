b9a256f347b3f8872a0cfe9d2bedca28241230ad
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-04-07 21:51:33 -0700

    app.jsx: updated to use apollo client and provider, set up graphQL endpoint, use JWT for authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Request middleware that will attach the JWT token to every request as an authorization header
+const authLink = setContext((_, { headers }) => {
+  // Get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so htppLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <ApolloProvider client={client}>
+      <Outlet />
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
