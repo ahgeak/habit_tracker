623c9394d2aaba78d8a58869a8db92bebec82854
Anna Chuapetcharasopon <143050387+ahgeak@users.noreply.github.com>
2024-04-15 21:14:24 -0700

    connected CreateHabit to home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,7 +1,10 @@
+import CreateHabit from "../components/CreateHabit";
+
 const Home = () => {
   return (
     <div>
         {/* elements that I will display */}
+        <CreateHabit />
         <p>Testing</p>
     </div>
     );
